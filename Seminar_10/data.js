6e69e837198402f2f0c589cf737dc7e579d19400
Aнастасия <007badgerl@gmail.com>
2023-09-13 16:32:49 +0300

    save

---
+++ Seminar_10/data.js
@@ -0,0 +1,74 @@
+export const data = `[
+{
+    "id": 1,
+    "name": "Rick Sanchez",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Male",
+    "location": {
+        "name": "Citadel of Ricks",
+        "url": "https://rickandmortyapi.com/api/location/3"
+    },
+    "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+},
+{
+    "id": 2,
+    "name": "Morty Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Male",
+    "location": {
+        "name": "Citadel of Ricks",
+        "url": "https://rickandmortyapi.com/api/location/3"
+    },
+    "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+},
+{
+    "id": 3,
+    "name": "Summer Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Female",
+    "location": {
+        "name": "Earth (Replacement Dimension)",
+        "url": "https://rickandmortyapi.com/api/location/20"
+    },
+    "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+},
+{
+    "id": 4,
+    "name": "Beth Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Female",
+    "location": {
+        "name": "Earth (Replacement Dimension)",
+        "url": "https://rickandmortyapi.com/api/location/20"
+    },
+    "image": "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+},
+{
+    "id": 5,
+    "name": "Jerry Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Male",
+    "location": {
+        "name": "Earth (Replacement Dimension)",
+        "url": "https://rickandmortyapi.com/api/location/20"
+    },
+    "image": "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+},
+{
+    "id": 6,
+    "name": "Abadango Cluster Princess",
+    "status": "Alive",
+    "species": "Alien",
+    "gender": "Female",
+    "location": {
+        "name": "Abadango",
+        "url": "https://rickandmortyapi.com/api/location/2"
+    },
+    "image": "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+}
+]`
---
+++ Seminar_10/index.js
@@ -0,0 +1,4 @@
+import {data} from "./data.js";
+const result = JSON.parse(data);
+
+console.log(data);
