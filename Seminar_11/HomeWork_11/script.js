56f772c97a39118ae4db1c221cfa544c78415dfe
Anastasiya <007-badgerl@rambler.ru>
2023-09-18 23:00:00 +0300

    Домашнее задание 11

--- Seminar_11/HomeWork_11/script.js
+++ Seminar_11/HomeWork_11/script.js
@@ -1,29 +1,26 @@
 async function fetchData() {
   try {
-    const response = await fetchData('data.json');
-    console.log(response);
+    const response = await fetch('data.json');
     if (!response.ok) {
-      throw new Error('не удалось получть данные с json')
+      throw new Error('не удалось получть данные с json');
     }
     const data = await response.json();
     console.log(data);
-    const productBox = document.querySelector('.content');
+    const productBox = document.querySelector('.cards');
     data.forEach(({ image, title, subtitle, prise }) => {
       const productEl = `
-          <div class="card">
-            <div class="img__Card">
-              <img src="${image}" />
-            </div>
-            <div class="text__Card">
-              <h4>"${title}"</h4>
-              <h3>
-              "${subtitle}"
-              </h3>
-              <p>"${prise}"</p>
-            </div>
-          </div>`;
-      productBox.insertAdjacentHTML('beforeend', productEl);
-    })
+        <div class="card">
+          <div class="img__Card">
+            <img src="${image}">
+          </div>
+          <div class="text__Card">
+            <h4>"${title}"</h4>
+            <h3>"${subtitle}"</h3>
+            <p>"${prise}"</p>
+          </div>
+        </div>`;
+      productBox.insertAdjacentHTML("beforeend", productEl);
+    });
   } catch (error) {
     console.error(error);
   }
